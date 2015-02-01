ce9b4050c07410120edd55dbacdce3e78cc0ad04
Debakel <git@mrtz.me>
2015-02-01 14:33:32 +0100

    add date

--- feedDiasp/PostDB.py
+++ feedDiasp/PostDB.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+from datetime import datetime
 class PostDB:
 	# Textdatei mit IDs bereits veröffentlicher Einträge
 	def __init__(self, filename):
@@ -8,9 +9,11 @@ class PostDB:
 		self.liste = self.db.read()
 	def mark_as_posted(self, post_id):
 		self.liste = self.liste + '\n' + post_id
-		self.db.write(post_id+'\n')
+		self.db.write(str(datetime.today()) + ' ' + post_id+'\n')
+		self.db.flush()
 	def is_published(self, post_id):
 		if post_id in self.liste:
 			return True
 		else:
 			return False	
+
