f0ecaa8ade66ec234921337e168b190c4d297098
Debakel <git@mrtz.me>
2015-02-04 00:24:53 +0100

    JSON response

--- feedDiasp/web.py
+++ feedDiasp/web.py
@@ -1,11 +1,13 @@
-from bottle import route, run, template, post, get, request
-
 import diaspy
 from diaspy.errors import LoginError, DiaspyError
 from DB import Store, Feed
 import dataset
+import json
 
-	
+from bottle import route, run, template, post, get, request
+
+
+
 @post('/add')
 def add():
 	fb = request.forms.get('fb')
@@ -14,16 +16,11 @@ def add():
 	password=request.forms.get('password')
 	pod=request.forms.get('pod')
 	print username+password+pod
+
 	try:
 		c=diaspy.connection.Connection(pod=pod,username=username, password=password)
 		c.login()
-		success=True
-	#except LoginError as e:
-	#except DiaspyError as e:
-	except Exception as e:
-		success=False
-		return str(e)
-	if success:
+
 		db=Store()
 		if fb is not None and len(fb) > 1:
 			source_type='fb'
@@ -34,12 +31,16 @@ def add():
 		db = dataset.connect('sqlite:///mydatabase.db')
 		if db['user'].find_one(username=username, pod=pod, source=source) is None:
 			db['user'].insert(dict(source=source, username=username, password=password, pod=pod, source_type=source_type))
-			return 'OK'
+			success = True
+			message = 'Eintrag hinzugefuegt.'
 		else:
-			return 'Bereits in Datenbank: ' + username + ' auf ' + pod
+			success = False
+			message = 'Bereits in Datenbank  (' + username + ' auf ' + pod + ')' 	
+	except Exception as e:
+		success=False
+		message='Login nicht moeglich.'
 		
-	else:
-		return 'NE'
+	return json.dumps(dict(success=success, message=message))
 @get('/add')	
 @route('/')
 def index():
