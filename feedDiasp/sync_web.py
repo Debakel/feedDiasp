48b4013131124e4a8812c4dc78d502cc455056db
Debakel <git@mrtz.me>
2015-02-04 21:49:50 +0100

    update sync_web.py

--- feedDiasp/sync_web.py
+++ feedDiasp/sync_web.py
@@ -8,9 +8,9 @@ db = dataset.connect('sqlite:///mydatabase.db')
 for user in db['user']:
 	print user['username'] + '@' + user['pod']  + ': ' + user['source'] 
 	if user['source_type'] == 'fb':
-		print 'FB ausgelassen'
+		# Todo
+		pass
 	elif user['source_type'] == 'rss':
-		print 'RSS'
 		parser = RSSParser(user['source'])
 		diasp = FeedDiasp(parser=parser, username=user['username'], password=user['password'], pod=user['pod'], db='posts.db')
 		diasp.publish()
